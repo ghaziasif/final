1f72844b6b85f897b94b2993ad64d4d17d114889
ghaziasif <ghaziasif97@gmail.com>
2016-08-29 13:10:22 +0500

    made a folder "helper" and made a new class in it named "nhgenerator"

---
+++ ghaziprofessional/helper/nhgenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Context;
+
+namespace WebApplication3.helper
+{
+
+    static public class nhgenerator
+    {
+        // make sure this should be gerneated only once.
+        public static ISessionFactory _SF = null;
+        public static ISessionFactory GetSF()
+        {
+            try
+            {
+                if (_SF == null)
+                {
+                    var NHConfig = new NHibernate.Cfg.Configuration();
+
+                    NHConfig.DataBaseIntegration(delegate(NHibernate.Cfg.Loquacious.IDbIntegrationConfigurationProperties abc)
+                    {
+
+                        abc.ConnectionStringName = "DBCS";
+                        abc.Dialect<NHibernate.Dialect.MySQL55Dialect>();
+                        abc.Driver<NHibernate.Driver.MySqlDataDriver>();
+                        abc.Timeout = 60;
+                    }
+
+                    );
+
+                    //NHConfig.AddAssembly("JuneBatchMVC");
+                    NHConfig.AddAssembly(typeof(Student).Assembly);
+
+                    //when u get an error
+                    NHConfig.CurrentSessionContext<WebSessionContext>();
+
+
+                    _SF = NHConfig.BuildSessionFactory();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return _SF;
+        }
+    }
+}
